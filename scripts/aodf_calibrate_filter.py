9abc77c7c29d4feba47e67a1708703eb88e3e8bb
CHrlS98 <charles.poirier@usherbrooke.ca>
2023-11-01 11:20:02 -0400

    Fix todos for filter calibration

--- scripts/aodf_calibrate_filter.py
+++ scripts/aodf_calibrate_filter.py
@@ -27,8 +27,8 @@ def _build_arg_parser():
                         '[%(default)s]')
 
     p.add_argument('--crop_size', type=int, default=60,
-                   help='Width of the patch considered for calibration. '
-                        '[%(default)s]')
+                   help='Width of the patch considered for calibration '
+                        '(voxels). [%(default)s]')
 
     add_sh_basis_arg(p)
     return p
@@ -36,7 +36,7 @@ def _build_arg_parser():
 
 def _filter_sh_functional(in_sh, sh_order, sh_basis, full_basis,
                           sphere_str, spatial, align, angle, range,
-                          sf_range, j_invariance=False, channel_axis=-1):
+                          sf_range, j_invariance=False, legacy=True):
     # if a parameter is None, disable the filter
     disable_spatial = disable_align = disable_angle = disable_range = False
     if spatial is None:
@@ -57,7 +57,7 @@ def _filter_sh_functional(in_sh, sh_order, sh_basis, full_basis,
 
     # initialize j-invariant filter
     afilter = AsymmetricFilter(sh_order=sh_order, sh_basis=sh_basis,
-                               legacy=True, full_basis=full_basis,
+                               legacy=legacy, full_basis=full_basis,
                                sphere_str=sphere_str,
                                sigma_spatial=spatial,
                                sigma_align=align,
@@ -74,14 +74,14 @@ def _filter_sh_functional(in_sh, sh_order, sh_basis, full_basis,
     return filtered_sh
 
 
-def _compute_symmetric_sf(in_sh, sphere, sh_basis, full_basis):
+def _compute_symmetric_sf(in_sh, sphere, sh_basis, legacy=True):
     order, full = get_sh_order_and_fullness(in_sh.shape[-1])
     if full:
         _, l_list = sph_harm_ind_list(order, full_basis=full)
         in_sh = in_sh[..., l_list % 2 == 0]
     # reconstruct symmetric part of signal
     return sh_to_sf(in_sh, sphere, order, sh_basis,
-                    full_basis=False, legacy=True)
+                    full_basis=False, legacy=legacy)
 
 
 def mse(image1, image2):
@@ -98,23 +98,24 @@ def _product_from_dict(dictionary):
 
 
 def crop_center(data, size):
+    """
+    Extract a patch of `size` voxels width at center of `data`.
+    """
     return data[data.shape[0]//2-size//2:data.shape[0]//2+size//2,
                 data.shape[1]//2-size//2:data.shape[1]//2+size//2,
                 data.shape[2]//2-size//2:data.shape[2]//2+size//2]
 
 
 def grid_search(in_sh, denoise_func, aggregate_func, tested_parameters, crop_size):
-    _, in_full = get_sh_order_and_fullness(in_sh.shape[-1])
-
     # this will be the input symmetric SF
-    noisy_input = aggregate_func(in_sh, full_basis=in_full)
+    noisy_input = aggregate_func(in_sh)
     losses = []
     parameters = []
     min_loss = np.inf
 
     for args in _product_from_dict(tested_parameters):
         out_sh = denoise_func(in_sh, **args)
-        denoised = aggregate_func(out_sh, full_basis=True)
+        denoised = aggregate_func(out_sh)
         loss = mse(crop_center(noisy_input, crop_size),
                    crop_center(denoised, crop_size))
 
@@ -130,6 +131,12 @@ def grid_search(in_sh, denoise_func, aggregate_func, tested_parameters, crop_siz
     return parameters, losses
 
 
+def assert_crop_volume(parser, crop_size, volume_shape):
+    volume_shape = np.asarray(volume_shape)
+    if (volume_shape < crop_size).any():
+        parser.error('Crop size with padding exceeds volume dimensions.')
+
+
 def main():
     parser = _build_arg_parser()
     args = parser.parse_args()
@@ -139,24 +146,22 @@ def main():
 
     _, ext = os.path.splitext(args.out_table)
     if ext != '.csv':
-        parser.error('Invalid extension for output.')
+        parser.error('Invalid extension for output. Expected .csv.')
 
     sh_basis, legacy = parse_sh_basis(args.sh_basis)
-
-    if not legacy:
-        parser.error('Modern SH bases not supported yet. Use a legacy SH basis.')
 
     # TODO: These values should be set by argparser
     tested_sigmas = {
         'spatial':[1.0, 1.2, 1.4, 1.6, 1.8],
         'align': [0.4, 0.6, 0.8, 1.0, 1.2],
         'range': [0.2, 1.0, None],
-        'angle': [None, 0.1, 0.2],
-        'channel_axis': [-1] # TODO: Remove. This was useful when using skimage.
+        'angle': [None, 0.1, 0.2]
     }
 
     # crop the data and add padding by maximum filter width
     crop_size = args.crop_size + 2*int(3.0*np.max(tested_sigmas['spatial']) + 0.5)
+    assert_crop_volume(parser, crop_size, in_sh.shape[:3])
+
     in_sh = in_sh[in_sh.shape[0]//2-crop_size//2:in_sh.shape[0]//2+crop_size//2,
                   in_sh.shape[1]//2-crop_size//2:in_sh.shape[1]//2+crop_size//2,
                   in_sh.shape[2]//2-crop_size//2:in_sh.shape[2]//2+crop_size//2]
@@ -164,17 +169,22 @@ def main():
     order, full = get_sh_order_and_fullness(in_sh.shape[-1])
     sh_to_sf_sphere = get_sphere(args.sphere_filter)
 
-    # filtering function
+    # compute SF range for scaling sigma_range parameter
     sf_range = get_sf_range(in_sh, order, full, args.sphere_highres)
-    print('sf range:', sf_range)
+    print('SF range is:', sf_range)
+
+    # filtering function
     denoise_func = partial(_filter_sh_functional, sh_order=order,
                            sh_basis=sh_basis, full_basis=full,
                            sphere_str=args.sphere_filter, sf_range=sf_range,
-                           j_invariance=True)
+                           j_invariance=True, legacy=legacy)
 
-    aggregate_func = partial(_compute_symmetric_sf, sphere=sh_to_sf_sphere,
-                             sh_basis=sh_basis)
+    aggregate_func = partial(_compute_symmetric_sf,
+                             sphere=sh_to_sf_sphere,
+                             sh_basis=sh_basis,
+                             legacy=legacy)
 
+    # launch grid search
     params, losses = grid_search(in_sh, denoise_func=denoise_func,
                                  aggregate_func=aggregate_func,
                                  tested_parameters=tested_sigmas,
